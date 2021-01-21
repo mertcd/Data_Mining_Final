532db3f1354b0c2a07a415626989e979cbe94b50
mertcd <46134713+mertcd@users.noreply.github.com>
2021-01-21 15:05:19 +0300

    Row will be selected randomly from the data.

--- final.py
+++ final.py
@@ -1,9 +1,10 @@
 import random
 import warnings
-
+from imblearn.over_sampling import SMOTE
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import mean_squared_error, accuracy_score
 from sklearn.preprocessing import Normalizer
@@ -13,6 +14,17 @@ warnings.filterwarnings("ignore")
 
 file = open("creditcard.csv", "r")
 filew = open("creditpositive.csv", "w")
+
+df = pd.read_csv('creditcard.csv')
+
+
+
+"""
+Data is highly unbalanced because of that I will be applying undersampling technique.
+
+Firstly I attempted to do undersampling on imbalanced data.
+500 random numbers will be selected and these rows will be taken as undersampled
+"""
 
 randomNumbers = []
 for i in range(500):
@@ -33,7 +45,7 @@ for line in file:
     if int(ls[-1][1]) == 1:
 
         filew.write(",".join(ls))
-    elif count in randomNumbers:
+    elif count in randomNumbers:#Random rows will be selected for undersampling
         filew.write(",".join(ls))
 df = pd.read_csv('creditpositive.csv')
 
@@ -64,7 +76,7 @@ axis2.plot(x_test[y_test == 0, 1], x_test[y_test == 0, 29], '+b')
 axis2.plot(x_test[y_test == 1, 1], x_test[y_test == 1, 29], '+g')
 
 plt.show()
-fig.savefig("OutlierDetection.pdf", dpi=None,
+fig.savefig("clustering on credit cart data.pdf", dpi=None,
             facecolor='w', edgecolor='w', format="pdf")
 
 req = LogisticRegression().fit(x_train,y_train)
@@ -74,7 +86,7 @@ y_predict = req.predict(x_test)
 print("Accuracy score on balanced data with logistic regression "+str(accuracy_score(y_test,y_predict)))
 fig2, (axis3,axis4 ) = plt.subplots(1,2)
 
-axis4.set_title("logistic regresyon applied")
+axis4.set_title("logistic regresyon applied and accuracy score:"+str(accuracy_score(y_test,y_predict)))
 axis4.plot(x_test[y_predict == 0, 1], x_test[y_predict == 0, 29], '+b')
 axis4.plot(x_test[y_predict == 1, 1], x_test[y_predict == 1, 29], '+g')
 
@@ -83,3 +95,10 @@ axis3.plot(x_test[y_test == 0, 1], x_test[y_test == 0, 29], '+b')
 axis3.plot(x_test[y_test == 1, 1], x_test[y_test == 1, 29], '+g')
 
 plt.show()
+
+
+clf = RandomForestClassifier(max_depth=2, random_state=0).fit(x_train,y_train)
+
+y_predict = clf.predict(x_test)
+
+print("Accuracy score for Random Forest classiffier: " + str(accuracy_score(y_test,y_predict)))
